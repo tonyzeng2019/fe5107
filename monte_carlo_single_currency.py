cedd383524dc55e6fcdabcd0ce8dcc72e3868812
tony_zeng <53079755Zjp@>
2021-04-10 14:45:45 +0800

    added codes to run monte carlo on past 7 data

--- monte_carlo_single_currency.py
+++ monte_carlo_single_currency.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 
 return_dataset = pd.read_csv('processed_data/returns.csv')
-return_dataset_2018 = pd.read_csv('processed_data/returns_2018.csv')
 np.random.seed(42)
 # get the index from the df for 2018-01-01
 i_2018 = return_dataset[return_dataset['Date']=='2018-01-01'].index[0]
@@ -17,17 +16,17 @@ df_mc_95 = pd.DataFrame({"Date":[],
 
 # Run Monte Carlo Simulations based on the predicted Volatility & assumed mean 0
 # number of simulations
-for n_sims in [1000,10000,10000]:
+for n_sims in [1000,10000,100000]:
     for i in range (i_2018, T):
         # past 7 days data
         train_data = return_all[i_2018 - 7:i_2018]
         # mean for respective crypto-currency
-        mean_all = np.mean(train_data)
+        mean_all = np.mean(train_data, axis=0)
         mean_btc = mean_all[0]
         mean_eth = mean_all[1]
         mean_xrp = mean_all[2]
         # std for respective crypto-currency
-        std_all = np.std(train_data)
+        std_all = np.std(train_data, axis=0)
         std_btc = std_all[0]
         std_eth = std_all[1]
         std_xrp = std_all[2]
@@ -37,7 +36,7 @@ for n_sims in [1000,10000,10000]:
         v_var_95_btc = -np.percentile(sim_returns_btc, 5)
         v_var_95_eth = -np.percentile(sim_returns_eth, 5)
         v_var_95_xrp = -np.percentile(sim_returns_xrp, 5)
-        returns_btc_eth_xrp = return_dataset_2018.iloc[i,:]
+        returns_btc_eth_xrp = return_dataset.iloc[i,:]
         date = returns_btc_eth_xrp[0]
         v_return_btc = returns_btc_eth_xrp[1]
         v_return_eth = returns_btc_eth_xrp[2]
